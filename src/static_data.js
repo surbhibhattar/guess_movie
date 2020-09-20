aa65b8de7fe55f7b4ba08f6de2c6ccb14d6a3dac
Surbhi <surbhibhattar92@gmail.com>
2020-09-20 18:27:37 +0530

    first commit

---
+++ src/static_data.js
@@ -0,0 +1,17 @@
+export let movies = [
+  {
+    name: "Lord Of The Rings",
+    hint:
+      "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+  },
+  {
+    name: "Extremely Loud And Incredibly Close",
+    hint:
+      "A nine-year-old amateur inventor, Francophile, and pacifist searches New York City for the lock that matches a mysterious key left behind by his father, who died in the World Trade Center on September 11, 2001.",
+  },
+  {
+    name: "Gravity",
+    hint:
+      "Two astronauts work together to survive after an accident leaves them stranded in space.",
+  },
+];
